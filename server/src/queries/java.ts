163bc423e32efccdb2041ef30697f87e6d74abf6
Johannes Rieken <johannes.rieken@gmail.com>
2021-09-10 18:10:53 +0200

    java - add bracket-less while body as scope

--- server/src/queries/java.ts
+++ server/src/queries/java.ts
@@ -74,6 +74,7 @@ const scopes = `
 (for_statement) @scope
 (if_statement consequence: (_) @scope)
 (if_statement alternative: (_) @scope)
+(while_statement body: (_) @scope)
 (try_statement (block) @scope)
 (catch_clause) @scope
 (block) @scope
