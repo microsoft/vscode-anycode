f7ded8f04ec1896b100f54f18990243ae5d66a94
Johannes Rieken <johannes.rieken@gmail.com>
2021-09-28 16:07:42 +0200

    update rust references for call expressions and scoped identifiers

--- server/src/queries/rust/references.scm
+++ server/src/queries/rust/references.scm
@@ -1,4 +1,4 @@
 (field_identifier) @ref.field
 (type_identifier) @ref.type
-(call_expression (identifier)) @ref.call
-(scoped_identifier (identifier)) @ref
+(call_expression (identifier) @ref.call) 
+(scoped_identifier name: (identifier) @ref)
