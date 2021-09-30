a2afc3c45b0d13951b43f08e7d865b1c8a6e50a0
Johannes Rieken <johannes.rieken@gmail.com>
2021-09-30 09:31:38 +0200

    add macro invocations and marco "arguments" to rust usage query

--- server/src/queries/rust/references.scm
+++ server/src/queries/rust/references.scm
@@ -2,3 +2,5 @@
 (type_identifier) @ref.type
 (call_expression (identifier) @ref.call) 
 (scoped_identifier name: (identifier) @ref)
+(macro_invocation macro: (identifier) @ref)
+((macro_invocation (token_tree (identifier) @ref)))
