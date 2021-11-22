49abf5ad62ceb97b04269074ce5b3c653fb1793d
Johannes Rieken <johannes.rieken@gmail.com>
2021-11-22 11:30:38 +0100

    a few more classified references

--- server/src/queries/java/references.scm
+++ server/src/queries/java/references.scm
@@ -1,11 +1,11 @@
 (method_invocation
-	name: (identifier) @ref.call)
+	name: (identifier) @ref.method)
 (interface_type_list
-	(type_identifier) @ref.type)
+	(type_identifier) @ref.interface)
 (superclass 
-	(type_identifier) @ref.type)
+	(type_identifier) @ref.class)
 (object_creation_expression
-  type: (type_identifier) @ref.type)
- (type_identifier) @ref
+  type: (type_identifier) @ref.class)
+ (type_identifier) @ref.class.interface.enum
 (field_access
 	field: (identifier) @ref.field)
--- server/src/queries/rust/references.scm
+++ server/src/queries/rust/references.scm
@@ -1,6 +1,6 @@
-(field_identifier) @ref.field
-(type_identifier) @ref.type
-(call_expression (identifier) @ref.call) 
+(field_identifier) @ref.field.method
+(type_identifier) @ref.interface.struct.class
+(call_expression (identifier) @ref.function) 
 (scoped_identifier name: (identifier) @ref)
 (macro_invocation macro: (identifier) @ref)
 ((macro_invocation (token_tree (identifier) @ref)))
