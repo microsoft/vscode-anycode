a481270f243bca400a7352ba2000f5d4b7fc86af
Johannes Rieken <johannes.rieken@gmail.com>
2021-11-23 18:30:51 +0100

    refine references-queries but don't use usages to scope definitions etc (yet?)

--- server/src/queries/typescript/references.scm
+++ server/src/queries/typescript/references.scm
@@ -1,5 +1,7 @@
-(property_identifier) @ref.field.method
 (type_identifier) @ref.class.interface.enum
 (new_expression
 	constructor: (identifier) @ref.class)
-(call_expression (identifier) @ref.function)
+(call_expression [
+	(identifier) @ref.function
+ 	(member_expression property: (property_identifier) @ref.function)])
+(property_identifier) @ref.field.method
