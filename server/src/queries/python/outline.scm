38b3d8a18ef106d9edb9e47a09740ff9319bbc1d
Johannes Rieken <johannes.rieken@gmail.com>
2021-12-13 15:34:45 +0100

    add module variables to python outline

--- server/src/queries/python/outline.scm
+++ server/src/queries/python/outline.scm
@@ -1,7 +1,9 @@
 (class_definition
-	name: (identifier) @class.name
-) @class
+	name: (identifier) @class.name) @class
 
 (function_definition
-	name: (identifier) @function.name
-) @function
+	name: (identifier) @function.name) @function
+
+(module
+	(expression_statement 
+		(assignment left: (identifier) @var)))
