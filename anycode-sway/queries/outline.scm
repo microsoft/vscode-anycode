0b5df23c008f1f6fe5aab2bd5dac9ccc2a7cf841
Sophie <sophiedankel@gmail.com>
2023-03-06 18:26:55 -0800

    Add sway language

---
+++ anycode-sway/queries/outline.scm
@@ -0,0 +1,41 @@
+(mod_item
+	name: (identifier) @module.name) @module
+
+(function_item
+	name: (identifier) @function.name) @function
+
+(union_item
+	name: (type_identifier) @struct.name) @struct
+
+(field_declaration
+	name: (field_identifier) @field.name) @field
+
+(struct_item
+	name: (type_identifier) @struct.name) @struct
+
+(enum_item
+	name: (type_identifier) @enum.name) @enum
+
+(enum_variant
+	name: (identifier) @enumMember.name) @enumMember
+
+(trait_item
+	name: (type_identifier) @interface.name) @interface
+
+(function_signature_item
+	name: (identifier) @function.name) @function
+
+(const_item
+	name: (identifier) @constant.name) @constant
+
+(static_item
+	name: (identifier) @constant.name) @constant
+
+(type_item
+	name: (type_identifier) @interface.name) @interface
+
+(impl_item 
+	. [(generic_type) (type_identifier)] @class.name) @class
+
+(foreign_mod_item
+	(extern_modifier (string_literal) @namespace.name)) @namespace
