6446513020c09fafc19933fff133b583654f2e1e
Robo <hop2deep@gmail.com>
2024-04-06 00:18:11 +0900

    chore: revert "fix(ci): downgrade tree-sitter-typescript"  (#93)
    * temp: update pipeline template
    
    * Revert "update query files according to grammar changes"
    
    This reverts commit edadadef865a61621cd082702be0e1f214970f6e.
    
    * Revert "fix(ci): downgrade tree-sitter-typescript"
    
    This reverts commit e9329c34f7f49a3c769dbb0534c296eb930da43f.
    
    * temp: print env
    
    * fix: force native addons build from source
    
    * Revert "temp: update pipeline template"
    
    This reverts commit 1e368dfd8e9c17a193c1de37a3e9e97db4c6db05.

--- anycode-typescript/queries/locals.scm
+++ anycode-typescript/queries/locals.scm
@@ -1,11 +1,9 @@
 (method_definition) @scope
 (function_declaration) @scope
-;; (function_expression) @scope
-(function) @scope
+(function_expression) @scope
 (arrow_function) @scope
 [(class_body) (enum_body)] @scope
-;; (interface_declaration body: (interface_body) @scope)
-(interface_declaration body: (object_type) @scope)
+(interface_declaration body: (interface_body) @scope)
 (for_statement) @scope
 (if_statement consequence: (_) @scope)
 (if_statement alternative: (_) @scope)
@@ -15,8 +13,7 @@
 (statement_block) @scope
 
 (function_declaration name: (identifier) @local.escape)
-;; (function_expression name: (identifier) @local.escape)
-(function name: (identifier) @local.escape)
+(function_expression name: (identifier) @local.escape)
 (required_parameter (identifier) @local)
 (optional_parameter (identifier) @local)
 (catch_clause parameter: (identifier) @local)
